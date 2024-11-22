bbbc8c081157e3da9689c184b4ee21af9d425013
Aung Myat Min <64914752+AndrewAung11@users.noreply.github.com>
2024-11-22 23:01:49 +0630

    Split files for classes

---
+++ src/com/memory_game/Game.java
@@ -0,0 +1,10 @@
+package com.memory_game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JPanel{
+    public Game(int option) {
+        this.setPreferredSize(new Dimension(600, 600));
+    }
+}
---
+++ src/com/memory_game/StartMenu.java
@@ -0,0 +1,10 @@
+package com.memory_game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartMenu extends JPanel {
+    public StartMenu() {
+        this.setPreferredSize(new Dimension(500, 200));
+    }
+}
