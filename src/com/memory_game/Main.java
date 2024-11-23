0674105747c376eda7f9d4a9706e4573d95632a1
Aung Myat Min <64914752+AndrewAung11@users.noreply.github.com>
2024-11-23 20:18:22 +0630

    Start menu

--- src/com/memory_game/Main.java
+++ src/com/memory_game/Main.java
@@ -7,17 +7,27 @@ public class Main {
 
     private static JFrame f;
 
+    private static StartMenu startMenuS;
+    private static Game gameS;
+
     public static void main(String[] args) {
         Main.f = new JFrame("Memory game");
 
-
+        startMenuS = new StartMenu();
+        Main.f.add(startMenuS);
 
         Main.f.setVisible(true);
-        Main.f.setSize(500, 500);
+        Main.f.setSize(1000, 1000);
         Main.f.setResizable(false);
         Main.f.setLocationRelativeTo(null);
         Main.f.pack();
     }
 
+    public static void changeToGame(int option) {
+        Main.f.remove(startMenuS);
 
+        gameS = new Game(option);
+        Main.f.add(gameS);
+        Main.f.pack();
+    }
 }
--- src/com/memory_game/StartMenu.java
+++ src/com/memory_game/StartMenu.java
@@ -6,5 +6,39 @@ import java.awt.*;
 public class StartMenu extends JPanel {
     public StartMenu() {
         this.setPreferredSize(new Dimension(500, 200));
+
+        // Header
+        JPanel secTitle = new JPanel();
+        secTitle.setPreferredSize(new Dimension(500, 75));
+        JLabel title = new JLabel("Choose a mode:");
+        title.setFont(new Font("Apotos", Font.PLAIN, 30));
+        title.setHorizontalTextPosition(JLabel.CENTER);
+        title.setVerticalTextPosition(JLabel.CENTER);
+        secTitle.add(title);
+
+        //Buttons
+        JPanel secButtons = new JPanel();
+        secButtons.setPreferredSize(new Dimension(500, 100));
+        secButtons.setLayout(new GridLayout(1, 3));
+
+        final JButton[] modes = new JButton[3];
+        String[] list = {"Easy", "Normal", "Difficult"};
+        for (int i = 0; i < 3; i++) {
+            modes[i] = new JButton(list[i]);
+
+            int finalI = i;
+            modes[i].addActionListener(e-> Main.changeToGame(finalI));
+
+            modes[i].setFocusable(false);
+            modes[i].setForeground(Color.black);
+            modes[i].setBackground(Color.white);
+            modes[i].setFont(new Font("Consolas", Font.PLAIN, 20));
+
+            secButtons.add(modes[i]);
+        }
+
+        // Add for display
+        this.add(secTitle);
+        this.add(secButtons);
     }
 }
