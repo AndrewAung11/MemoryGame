5538fc5777bffac6c84e083fc670e6d435902b29
Aung Myat Min <64914752+AndrewAung11@users.noreply.github.com>
2024-11-24 16:30:09 +0630

    Template for right side panel of game screen

--- src/com/memory_game/Game.java
+++ src/com/memory_game/Game.java
@@ -36,18 +36,63 @@ public class Game extends JPanel{
         this.add(rightSide);
     }
 
+
+    // Modes
     private void easy() {
         leftSide.setPreferredSize(new Dimension(width-150, height));
         rightSide.setPreferredSize(new Dimension(150, height));
+        infoSec();
     }
 
     private void normal() {
         leftSide.setPreferredSize(new Dimension(width-150, height));
         rightSide.setPreferredSize(new Dimension(150, height));
+        infoSec();
     }
 
     private void difficult() {
         leftSide.setPreferredSize(new Dimension(width-150, height));
         rightSide.setPreferredSize(new Dimension(150, height));
+        infoSec();
+    }
+
+    // display
+    private void infoSec() {
+        rightSide.setLayout(new GridLayout(3, 1));
+
+        JPanel timerSec = new JPanel();
+        JLabel timerLabel = new JLabel("Timer:");
+        timerLabel.setFont(new Font("Apotos", Font.PLAIN, 20));
+        JLabel timerWork = new JLabel("00:00:00");
+        timerWork.setFont(new Font("Consolas", Font.PLAIN, 20));
+        timerSec.setLayout(new GridLayout(4, 1));
+        timerSec.add(timerLabel);
+        timerSec.add(timerWork);
+        timerSec.add(new JPanel());
+        timerSec.add(new JPanel());
+
+        JPanel recordSec = new JPanel();
+        JLabel recordLabel = new JLabel("Fastest record:");
+        recordLabel.setFont(new Font("Apotos", Font.PLAIN, 20));
+        JLabel recorded = new JLabel("00:00:00");
+        recorded.setFont(new Font("Consolas", Font.PLAIN, 20));
+        recordSec.setLayout(new GridLayout(4, 1));
+        recordSec.add(recordLabel);
+        recordSec.add(recorded);
+        recordSec.add(new JPanel());
+        recordSec.add(new JPanel());
+
+        JPanel buttonSec = new JPanel();
+        JButton startBtn = new JButton("Start");
+        startBtn.setForeground(Color.black);
+        startBtn.setBackground(Color.white);
+        startBtn.setFont(new Font("Arial", Font.PLAIN, 30));
+        buttonSec.setLayout(new GridLayout(3, 1));
+        buttonSec.add(new JPanel());
+        buttonSec.add(startBtn);
+
+        rightSide.add(timerSec);
+        rightSide.add(recordSec);
+        rightSide.add(buttonSec);
     }
 }
